cc6b20a03467d9f0ba993c646e54151850a69923
Dirk Neumann <dirk@caltech.edu>
2014-07-01 19:32:04 -0700

    tuple-gymnastics docs

--- tuplenastics.py
+++ tuplenastics.py
@@ -1,3 +1,6 @@
+'''
+.flatMap(f_skip(lambda x: x['text'] if 'text' in x else None))
+'''
 def f_skip(f):
     def _f(x):
         fx = f(x)
@@ -5,18 +8,18 @@ def f_skip(f):
     return _f
 
 
+'''
+.map(add_(lambda x: x['key']]))
+'''
 def add_(f):
     def _f(x):
         return (f(x), x)
     return _f
 
 
-def _add(f):
-    def _f(x):
-        return (x, f(x))
-    return _f
-
-
+'''
+.flatMap(add_skip(lambda x: x['value'] if 'value' in x else None]))
+'''
 def add_skip(f):
     def _f(x):
         fk = f(x)
@@ -24,18 +27,27 @@ def add_skip(f):
     return _f
 
 
+'''
+.map(_add(lambda x: x['value']]))
+'''
+def _add(f):
+    def _f(x):
+        return (x, f(x))
+    return _f
+
+
+'''
+.map(fk_(lambda key: key + 1))
+'''
 def fk_(f):
     def _f(x):
         return (f(x[0]), x[1])
     return _f
 
 
-def _fv(f):
-    def _f(x):
-        return (x[0], f(x[1]))
-    return _f
-
-
+'''
+.flatMap(fk_(lambda key: key + 1 if key > 0 else None))
+'''
 def fk_skip(f):
     def _f(x):
         fk = f(x[0])
@@ -43,6 +55,18 @@ def fk_skip(f):
     return _f
 
 
+'''
+.map(_fv(lambda value: value + 1))
+'''
+def _fv(f):
+    def _f(x):
+        return (x[0], f(x[1]))
+    return _f
+
+
+'''
+.flatMap(_fv_skip(lambda value: value['counter'] + 1 if 'counter' in value else None))
+'''
 def _fv_skip(f):
     def _f(x):
         fv = f(x[1])
@@ -50,6 +74,9 @@ def _fv_skip(f):
     return _f
 
 
+'''
+.flatMap(lambda x: x + 1 if x > 0 else None)
+)'''
 def skip_(f):
     def _f(x):
         fx = f(x)
@@ -64,6 +91,9 @@ def fk_skip(f):
     return _f
 
 
+'''
+.map(swap).sortByKey(...).map(swap)
+'''
 def swap(x):
     return (x[1], x[0])
 
